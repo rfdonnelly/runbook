93223fa19ac6c90b24b6155b8eb15de46e36cefe
Rob Donnelly <rfdonnelly@gmail.com>
2026-04-03 16:47:46 -0700

    refactor: tmux bits into their own module

--- src/runbook/cli.py
+++ src/runbook/cli.py
@@ -1,77 +1,11 @@
 from pathlib import Path
-import random
-import string
 import subprocess
 import sys
 from tempfile import NamedTemporaryFile
-import time
 
-import libtmux
-from libtmux.constants import PaneDirection
-
+from runbook.tmux import Tmux
 from runbook.reader import AsciidocReader, Markup, CodeBlock
 from runbook.writer import Writers, AsciidocWriter, MarkdownWriter
-
-
-def create_shellrc() -> NamedTemporaryFile:
-    shellrc = NamedTemporaryFile()
-    shellrc.write(
-        rb"export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '"
-    )
-    shellrc.flush()
-    return shellrc
-
-
-def create_pane(relative_to: libtmux.Pane, bashrc: str) -> libtmux.Pane:
-    return relative_to.split(
-        direction=PaneDirection.Right, shell=f"bash --rcfile {bashrc} -i"
-    )
-
-
-def create_marker() -> str:
-    length = 32
-    characters = string.ascii_letters + string.digits
-    random_characters = random.choices(characters, k=length)
-    random_string = "".join(random_characters)
-    return random_string
-
-
-def execute_and_capture_command(pane: libtmux.Pane, command: str) -> list[str]:
-    """
-    Executes a command in the provided pane and returns the output including
-    prompt and command.
-
-    Inserts a unique marker to mark the start of the output.  Command
-    completion is signaled via the tmux window name.
-    """
-
-    marker = create_marker()
-    saved_window_name = pane.window.name
-
-    pane.send_keys(f"echo {marker}")
-    pane.send_keys(f"{command}; tmux rename-window {marker}")
-
-    # Wait for command to complete. Completion is signaled by rename-window.
-    while pane.window.name != marker:
-        time.sleep(0.1)
-    pane.window.rename_window(saved_window_name)
-
-    text = pane.capture_pane(join_wrapped=True)
-    try:
-        start_index = text.index(marker)
-    except ValueError:
-        text = pane.capture_pane(start="-", join_wrapped=True)
-        start_index = text.index(marker)
-
-    # Extract output (everything after marker minus next prompt)
-    text = text[start_index + 1 : -1]
-
-    # Trim end marker
-    text[0], _ = text[0].rsplit(";")
-
-    text = [line + "\n" for line in text]
-
-    return text
 
 
 def edit_command(command: str) -> str:
@@ -93,12 +27,9 @@ def main() -> None:
     reader = AsciidocReader(ifile)
     writer = Writers([AsciidocWriter(ofile_adoc), MarkdownWriter(ofile_md)])
 
-    tmux = libtmux.Server()
+    tmux = Tmux()
 
-    host_pane = tmux.sessions[0].active_pane
-
-    shellrc = create_shellrc()
-    target_pane = create_pane(host_pane, shellrc.name)
+    target_pane = tmux.create_pane(tmux.host_pane)
 
     while chunk := reader.next_chunk():
         match chunk:
@@ -121,8 +52,8 @@ def main() -> None:
                                 case "e":
                                     command = edit_command(command)
                                     commands.append(command + "\n")
-                                    capture = execute_and_capture_command(
-                                        target_pane, command
+                                    capture = target_pane.execute_and_capture_command(
+                                        command
                                     )
                                     captures.extend(capture)
                                 case "n":
@@ -132,8 +63,8 @@ def main() -> None:
                                     continue
                                 case _:
                                     commands.append(command + "\n")
-                                    capture = execute_and_capture_command(
-                                        target_pane, command
+                                    capture = target_pane.execute_and_capture_command(
+                                        command
                                     )
                                     captures.extend(capture)
 
@@ -145,4 +76,4 @@ def main() -> None:
                     case "console":
                         pass
 
-    target_pane.kill()
+    target_pane.pane.kill()
---
+++ src/runbook/tmux.py
@@ -0,0 +1,87 @@
+import random
+import string
+from tempfile import NamedTemporaryFile
+import time
+
+import libtmux
+from libtmux.constants import PaneDirection
+
+
+class TmuxPane:
+    pane: libtmux.Pane
+
+    def __init__(self, pane: libtmux.Pane):
+        self.pane = pane
+
+    def execute_and_capture_command(self, command: str) -> list[str]:
+        """
+        Executes a command in the provided pane and returns the output including
+        prompt and command.
+
+        Inserts a unique marker to mark the start of the output.  Command
+        completion is signaled via the tmux window name.
+        """
+
+        marker = self.create_marker()
+        saved_window_name = self.pane.window.name
+
+        self.pane.send_keys(f"echo {marker}")
+        self.pane.send_keys(f"{command}; tmux rename-window {marker}")
+
+        # Wait for command to complete. Completion is signaled by rename-window.
+        while self.pane.window.name != marker:
+            time.sleep(0.1)
+        self.pane.window.rename_window(saved_window_name)
+
+        text = self.pane.capture_pane(join_wrapped=True)
+        try:
+            start_index = text.index(marker)
+        except ValueError:
+            text = self.pane.capture_pane(start="-", join_wrapped=True)
+            start_index = text.index(marker)
+
+        # Extract output (everything after marker minus next prompt)
+        text = text[start_index + 1 : -1]
+
+        # Trim end marker
+        text[0], _ = text[0].rsplit(";")
+
+        text = [line + "\n" for line in text]
+
+        return text
+
+    @staticmethod
+    def create_marker() -> str:
+        length = 32
+        characters = string.ascii_letters + string.digits
+        random_characters = random.choices(characters, k=length)
+        random_string = "".join(random_characters)
+        return random_string
+
+
+class Tmux:
+    shellrc: NamedTemporaryFile
+    server: libtmux.Server
+    host_pane: TmuxPane
+
+    def __init__(self):
+        self.shellrc = self.create_shellrc()
+        self.server = libtmux.Server()
+        self.host_pane = TmuxPane(self.server.sessions[0].active_pane)
+
+    @staticmethod
+    def create_shellrc() -> NamedTemporaryFile:
+        shellrc = NamedTemporaryFile()
+        shellrc.write(
+            rb"export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '"
+        )
+        shellrc.flush()
+        return shellrc
+
+    def create_pane(self, relative_to: TmuxPane) -> TmuxPane:
+        return TmuxPane(
+            relative_to.pane.split(
+                direction=PaneDirection.Right,
+                shell=f"bash --rcfile {self.shellrc.name} -i",
+            )
+        )
